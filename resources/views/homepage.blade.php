4a8ccc235a9b015fea23822eeecc7a0377978533
raphaelsrodrigues <raph@raph-web.com>
2021-08-26 15:36:44 +0200

    add homepage view

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>PHP Laravel web application</title>
+    <link href="{{ asset('css/gradients.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet" type="text/css">
+</head>
+
+<body class="">
+<div class="wrapper">
+
+    <h3 class="banner-text">{{ $quote }}</h3>
+
+</div>
+<script type="text/javascript" src="{{ asset('js/set-background.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,28 @@
 |
 */
 
-Route::get('/', function () {
+Route::get("/", function () {
 
     $brokers = [];
     $fees = [];
+
+    $quotes = [
+        "Do what you love, and period.",
+        "Listen to everyone, trust the people you admire, follow your heart.",
+        "It's not what they call you, it's what you answer to.",
+        "#govegan",
+        "Learn a vegan recipe.",
+        "Plenitude can be found in the smallest things.",
+        "Wear sunscreen.",
+        "Your dreams don't work if you don't.",
+    ];
 
     // Test database connection
     try {
         DB::connection()->getPdo();
 
-        $brokers = DB::select('select * from cryptofx');
-        $fees = DB::select('select * from fees');
+        $brokers = DB::select("select * from cryptofx");
+        $fees = DB::select("select * from fees");
 
     } catch (\Exception $e) {
         error_log("Database error: " . $e->getMessage());
@@ -29,9 +40,10 @@ Route::get('/', function () {
 
 
     $data = [
-        'brokers' => $brokers,
-        'fees' => $fees,
+        "brokers" => $brokers,
+        "fees" => $fees,
+        "quote" => $quotes[array_rand($quotes)]
     ];
 
-    return view('welcome', $data);
+    return view("homepage", $data);
 });
