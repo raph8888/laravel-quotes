ab410164741cd30137452e3b07106f5d46cfa803
raphaelsrodrigues <raph@raph-web.com>
2022-04-03 13:48:21 +0200

    adding spac to author

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -10,17 +10,17 @@ class HomepageController extends Controller {
         "\"Do what you love, and period.\" -raph",
         "\"Listen as much as one is able to <br> 
         to other people whose opinions you respect and look up to <br>
-        but in the end it has to come from you.\" -anna wintour",
-        "\"It's not what they call you. It's what you answer to.\" -kylie sonique love",
-        "\"#govegan\" -raph",
+        but in the end it has to come from you.\" <br>-anna wintour",
+        "\"It's not what they call you. It's what you answer to.\" <br>-kylie sonique love",
+        "\"Go vegan.\" <br>-raph",
         "Plenitude can be found in the smallest of things.",
         "Wear sunscreen.",
         "The hour is darkest before the dawn.",
         "Per aspera ad astra.",
-        "\"Take the red pill.\" -morpheus",
-        "\"What you are shouts so loudly in my ears <br> I cannot hear what you say.\" -ralph waldo emerson",
-        "\"Private victories precede public victories.\" -stephen covey",
-        "\"Remember the past, but don't live in it\" -raph"
+        "\"Take the red pill.\" <br>-morpheus",
+        "\"What you are shouts so loudly in my ears <br> I cannot hear what you say.\" <br>-ralph waldo emerson",
+        "\"Private victories precede public victories.\" <br>-stephen covey",
+        "\"Remember the past, but don't live in it\" <br>-raph"
     ];
 
     public static function homepageData(){
