1446b1a86dfe3037b38d3bc285bec74c9c55f66c
raph <rrodrigues@bitcoinmeester.nl>
2022-05-02 09:19:27 +0200

    fix quote

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -8,11 +8,9 @@ class HomepageController extends Controller {
 
     public static $quotes = [
         "\"Do what you love, and period.\" -raph",
-        "\"Listen as much as one is able to <br> 
-        to other people whose opinions you respect and look up to <br>
-        but in the end it has to come from you.\" <br>-anna wintour",
+        "\"Listen as much as one is able to, to other people whose opinions you respect and look up to. But in the end it has to come from you.\" <br>-anna wintour",
         "\"It's not what they call you. It's what you answer to.\" <br>-kylie sonique love",
-        "\"Go vegan.\" <br>-raph",
+        "#govegan",
         "Plenitude can be found in the smallest of things.",
         "Wear sunscreen.",
         "The hour is darkest before the dawn.",
@@ -20,7 +18,7 @@ class HomepageController extends Controller {
         "\"Take the red pill.\" <br>-morpheus",
         "\"What you are shouts so loudly in my ears <br> I cannot hear what you say.\" <br>-ralph waldo emerson",
         "\"Private victories precede public victories.\" <br>-stephen covey",
-        "\"Remember the past, but don't live in it\" <br>-raph"
+        "Remember the past, but don't live in it"
     ];
 
     public static function homepageData(){
