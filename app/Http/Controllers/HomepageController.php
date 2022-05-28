ee544ad5fdb29beef9f64576f3af96bc6561bc9e
raph <rrodrigues@bitcoinmeester.nl>
2022-05-28 10:42:32 +0200

    frankl quote

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -12,7 +12,7 @@ class HomepageController extends Controller {
         "\"Adoption over speculation.\" <br> -raph, about crypto",
         "\"#govegan\" <br> -raph, about food",
         "\"Listen as much as one is able to, to other people whose opinions you respect and look up to. But in the end it has to come from you.\" <br>-anna wintour",
-        "\"It's not what they call you. <br> It's what you answer to.\" <br>-w.c. Fields",
+        "\"It's not what they call you. <br> It's what you answer to.\" <br>-w.c. fields",
         "\"Plenitude can be found in the smallest of things.\"",
         "\"Wear sunscreen.\"",
         "\"The hour is darkest before the dawn.\"",
@@ -22,7 +22,8 @@ class HomepageController extends Controller {
         "\"Private victories precede public victories.\" <br>-stephen covey",
         "Remember the past, but don't live in it.",
         "\"Change is the only constant in life.\" <br> -heraclitus",
-        "\"If you don't believe in yourself, <br> nobody else will.\" <br>-kobe bryant"
+        "\"If you don't believe in yourself, <br> nobody else will.\" <br>-kobe bryant",
+        "\"When a person can't find a deep sense of meaning, <br> they distract themselves with pleasure.\" <br>-viktor e. frankl"
     ];
 
     public static function homepageData(){
