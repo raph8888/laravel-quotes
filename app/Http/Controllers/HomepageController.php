490f552c83f0c3fa3e17c7e5c0af79e9980deabc
raph <rrodrigues@bitcoinmeester.nl>
2022-06-23 10:27:48 +0200

    adding quote

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -10,7 +10,7 @@ class HomepageController extends Controller {
         "\"Do what you love, and period.\" <br> -raph",
         "\"It's not an alcohol problem, <br> it's an alcohol preference.\" <br> -raph, about red wine",
         "\"Adoption over speculation.\" <br> -raph, about crypto",
-        "\"#govegan\" <br> -raph, about food",
+        "\"Go vegan.\" <br> -raph",
         "\"Listen as much as one is able to, to other people whose opinions you respect and look up to. But in the end it has to come from you.\" <br>-anna wintour",
         "\"It's not what they call you. <br> It's what you answer to.\" <br>-w.c. fields",
         "\"Plenitude can be found in the smallest of things.\"",
@@ -23,7 +23,8 @@ class HomepageController extends Controller {
         "Remember the past, but don't live in it.",
         "\"Change is the only constant in life.\" <br> -heraclitus",
         "\"If you don't believe in yourself, <br> nobody else will.\" <br>-kobe bryant",
-        "\"When a person can't find a deep sense of meaning, <br> they distract themselves with pleasure.\" <br>-viktor e. frankl"
+        "\"When a person can't find a deep sense of meaning, <br> they distract themselves with pleasure.\" <br>-viktor e. frankl",
+        "\"Being horny is not chemistry. <br>-raph\""
     ];
 
     public static function homepageData(){
